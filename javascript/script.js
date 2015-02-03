218cd51ebc71ad81d5ec5a54fdb33f2808c86bef
dekkoShen <dekko.shen@gmail.com>
2015-02-04 01:50:03 +0800

    uses .load() to separate index.html and content

---
+++ javascript/script.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+
+	/* input the name of folder */
+	var folderName = "content" ;
+
+	/* set up the cover page */
+	var addr = "homeCover";
+	$(function(){
+		$('#content').load("./" + folderName + "/homeCover.html", function(){
+			$(this).children(':first').unwrap();       //'#content' within 'content' ==> need to unwrap
+		}); 
+	});
+
+	/* setup content-linking function */
+	var linkContent = function( linkID, fileName ){
+		var filePosition = "./" + folderName + "/"+ fileName + ".html"
+		$(linkID).click(function(){
+			if( addr != fileName) {
+				$('#content').load(filePosition, function() {
+					$(this).children(':first').unwrap();
+				});
+				addr = fileName;
+			};
+			//scroll back to origin
+			$('html, body').animate({scrollTop: 0},400);
+		});
+	};
+
+	/* content-linking */
+	linkContent( '#link_content_homeCover', "homeCover" );
+	linkContent( '#link_content_lesson', "lesson" );
+
+
+	/* preload images */
+	$.preloadImages = function() {
+		for (var i = 0; i < arguments.length; i++) {
+			$("<img />").attr("src", arguments[i]);
+		};
+	};
+	//preload_content_about
+	$.preloadImages(
+        "./images/line.jpg",
+        "./images/lesson_20141026.jpg"
+    );
+
+});
