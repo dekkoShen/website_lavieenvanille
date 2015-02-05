40e83b14c51918bdb811a86cf25a880d8728ab1c
dekkoShen <dekko.shen@gmail.com>
2015-02-05 15:46:56 +0800

    setup the footerHome function

--- javascript/script.js
+++ javascript/script.js
@@ -12,6 +12,7 @@ $(document).ready(function(){
 	});
 	$('#header .nav a').removeClass('active');
 
+
 	/* setup content-linking function */
 	var linkContent = function( linkID, fileName ){
 		var filePosition = "./" + folderName + "/"+ fileName + ".html"
@@ -28,8 +29,6 @@ $(document).ready(function(){
 					$(linkID).siblings().removeClass('active');	
 				};
 			};
-			//scroll back to origin
-			$('html, body').animate({scrollTop: 0},1);
 		});
 	};
 
@@ -41,6 +40,30 @@ $(document).ready(function(){
 	linkContent( '#link_content_lesson',    "lesson"    );
 	linkContent( '#link_content_blog',      "blog"      );
 	linkContent( '#link_content_contact',   "contact"   );
+
+
+	/* setup footerHome function */
+	var $footerHome = $('#link_content_homeCover_footerHome')
+	//scroll down and showup
+	$footerHome.css('opacity','0.2');
+	$(window).scroll(function(){
+		if ( $(document).scrollTop() >= $('#header').height() ) {
+			$footerHome.animate({opacity:0.9},{duration:100,queue:false});
+		} else{
+			$footerHome.animate({opacity:0.2},{duration:100,queue:false});
+		};
+	});
+	//hover effect
+	$footerHome.hover(function(){
+		$footerHome.find('img').css('width','3rem');
+	},function(){
+		$footerHome.find('img').css('width','2.5rem');
+	});
+	//seup function : scroll back to origin
+	$footerHome.click(function(){
+		$('html, body').animate({scrollTop: 0},400);
+	});
+
 
 
 	/* preload images */
