618a0a99b42860faf73fecb21fe42a36c958a712
dekkoShen <dekko.shen@gmail.com>
2015-02-05 21:39:27 +0800

    rewrite script.js for clarity

--- javascript/script.js
+++ javascript/script.js
@@ -1,50 +1,67 @@
 $(document).ready(function(){
 
-	/* input the name of folder */
-	var folderName = "content" ;
+	/*---------------------------------------------------------------------
+	define global parameters & input the information of file
+	---------------------------------------------------------------------*/
+	var folderName   = "content";      //fold for content html
+	var homeFileName = "homeCover";    //the file name of homepage html
+	var addr         = "";             //actived content name
 
-	/* set up the cover page */
-	var addr = "homeCover";
-	$(function(){
-		$('#content').load("./" + folderName + "/homeCover.html", function(){
+
+	/*---------------------------------------------------------------------
+	define some functions for hyperlink
+	---------------------------------------------------------------------*/
+	/* filePosition */
+	var filePosition = function (XfileName) {
+		return "./" + folderName + "/"+ XfileName + ".html" ;
+	};
+
+	/* link to the content html and adds 'active' class, except homepage */
+	var linkContent = function( XlinkID, XfileName ){
+		$('#content').load( filePosition(XfileName), function(){
 			$(this).children(':first').unwrap();       //'#content' within 'content' ==> need to unwrap
-		}); 
-	});
-	$('#header .nav a').removeClass('active');
+		});
+		addr = XfileName;
+		if ( XfileName===homeFileName ) {
+			$('#header .nav a').removeClass('active');
+		} else{
+			$(XlinkID).addClass('active');
+			$(XlinkID).siblings().removeClass('active');	
+		};
+	};
 
-
-	/* setup content-linking function */
-	var linkContent = function( linkID, fileName ){
-		var filePosition = "./" + folderName + "/"+ fileName + ".html"
-		$(linkID).click(function(){
-			if( addr != fileName) {
-				$('#content').load(filePosition, function() {
-					$(this).children(':first').unwrap();
-				});
-				addr = fileName;
-				if ( fileName==="homeCover" ) {
-					$('#header .nav a').removeClass('active');
-				} else{
-					$(linkID).addClass('active');
-					$(linkID).siblings().removeClass('active');	
-				};
+	/* setup click function for htperlink */
+	var clickLinkContent = function( XlinkID, XfileName ){
+		$(XlinkID).click(function(){
+			if( addr != XfileName) {
+				linkContent( XlinkID, XfileName );
 			};
 		});
 	};
 
-	/* content-linking */
-	linkContent( '#link_content_homeCover', "homeCover" );
-	linkContent( '#link_content_news',      "news"      );
-	linkContent( '#link_content_profile',   "profile"   );
-	linkContent( '#link_content_work',      "work"      );
-	linkContent( '#link_content_lesson',    "lesson"    );
-	linkContent( '#link_content_blog',      "blog"      );
-	linkContent( '#link_content_contact',   "contact"   );
+
+	/*---------------------------------------------------------------------
+	setup the click function for nav-hyperlink and homepage
+	---------------------------------------------------------------------*/
+	/* setup the homepage */
+	linkContent( '#link_content_homeCover', homeFileName );
+
+	/* click function for nav-hyperlink */
+	clickLinkContent( '#link_content_homeCover', homeFileName );
+	clickLinkContent( '#link_content_news',      "news"      );
+	clickLinkContent( '#link_content_profile',   "profile"   );
+	clickLinkContent( '#link_content_work',      "work"      );
+	clickLinkContent( '#link_content_lesson',    "lesson"    );
+	clickLinkContent( '#link_content_blog',      "blog"      );
+	clickLinkContent( '#link_content_contact',   "contact"   );
 
 
-	/* setup footerHome function */
+	/*---------------------------------------------------------------------
+	setup function for #footerHome 
+	---------------------------------------------------------------------*/
 	var $footerHome = $('#link_content_homeCover_footerHome')
-	//scroll down and showup
+
+	/* scroll down and showup */
 	$footerHome.css('opacity','0.2');
 	$(window).scroll(function(){
 		if ( $(document).scrollTop() >= $('#header').height() ) {
@@ -53,28 +70,33 @@ $(document).ready(function(){
 			$footerHome.animate({opacity:0.2},{duration:100,queue:false});
 		};
 	});
-	//hover effect
+
+	/* hover effect */
 	$footerHome.hover(function(){
 		$footerHome.find('img').css('width','3rem');
 	},function(){
 		$footerHome.find('img').css('width','2.5rem');
 	});
-	//seup function : scroll back to origin
+
+	/* seup function : scroll back to origin */
 	$footerHome.click(function(){
 		$('html, body').animate({scrollTop: 0},400);
 	});
 
 
-
-	/* preload images */
+	/*---------------------------------------------------------------------
+	preload images 
+	---------------------------------------------------------------------*/
+	/* setup preload function */
 	$.preloadImages = function() {
 		for (var i = 0; i < arguments.length; i++) {
 			$("<img />").attr("src", arguments[i]);
 		};
 	};
-	//preload_content_lesson
+	/* preload images */
 	$.preloadImages(
         "./images/line.png"
     );
+
 
 });
